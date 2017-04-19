e8e9e19e38e1861dbe2f6fb94e9e2247d615e20d
Riccardo Toso <riccardotoso@MBP.lan>
2017-04-19 12:54:38 +0200

    initial commit

---
+++ pyPassGen-v1.5.py
@@ -0,0 +1,55 @@
+import random
+import sys
+import argparse
+
+def main():
+    lowercase_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                        'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    uppercase_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+                        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    symbols = ['!', '£', '$', '%', '&' '(', ')', '=', '?', '^', '+', '*', '[', ']', '{', '}',
+            'ç', '@', '#', '§', '-', '_', '.', ':', ',', ';', '<', '>']
+    password = []
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--length', type=int, default=15, help='Specify the desired length of the password')
+    parser.add_argument('--num', type=int, default=3, help='Specify how many numbers must have the password')
+    parser.add_argument('--sym', type=int, default=2, help='Specify how many symbols must have the password')
+    args = parser.parse_args()
+
+    remaining_chars = args.length - args.num - args.sym
+    ul = random.randint(0, remaining_chars)
+    ll = remaining_chars - ul
+    done = False
+
+    while not done:
+        if ll > 0:
+            for a in range(0, ll):
+                password.append(lowercase_letters[random.randint(0, len(lowercase_letters)-1)])
+
+        if ul > 0:
+            for b in range(0, ul):
+                password.append(uppercase_letters[random.randint(0, len(uppercase_letters)-1)])
+        if args.num > 0:
+            for c in range(0, args.num):
+                password.append(numbers[random.randint(0, len(numbers)-1)])
+
+        if args.sym > 0:
+            for d in range(0, args.sym):
+                password.append(symbols[random.randint(0, len(symbols)-1)])
+
+        random.shuffle(password)
+        sh_pass = "".join(str(p) for p in password)
+        print("\n")
+        print(sh_pass)
+        print("\n")
+        exit = input("Are you satisfied? ('y' o 'n')  ")
+        if exit == "y":
+            done = True
+            print("Good Bye!")
+        else:
+            password = []
+
+if __name__ == '__main__':
+    main()
---
+++ pyPassGen-v1.py
@@ -0,0 +1,43 @@
+import random
+
+lowercase_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                     'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+uppercase_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+                     'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+symbols = ['!', '£', '$', '%', '&' '(', ')', '=', '?', '^', '+', '*', '[', ']', '{', '}',
+           'ç', '@', '#', '§', '-', '_', '.', ':', ',', ';', '<', '>']
+password = []
+
+lunghezza = int(input("Quanti caratteri deve avere la password desiderata?\n"))
+num = int(input("Quanti numeri deve contenere?\n"))
+sym = int(input("Quanti simboli?\n"))
+residuo = lunghezza - num - sym
+ul = random.randint(0, residuo)
+ll = residuo - ul
+soddisfatto = False
+
+while not soddisfatto:
+    if ll > 0:
+        for a in range(0, ll):
+            password.append(lowercase_letters[random.randint(0, len(lowercase_letters)-1)])
+
+    if ul > 0:
+        for b in range(0, ul):
+            password.append(uppercase_letters[random.randint(0, len(uppercase_letters)-1)])
+    if num > 0:
+        for c in range(0, num):
+            password.append(numbers[random.randint(0, len(numbers)-1)])
+
+    if sym > 0:
+        for d in range(0, sym):
+            password.append(symbols[random.randint(0, len(symbols)-1)])
+
+    random.shuffle(password)
+    sh_pass = "".join(str(p) for p in password)
+    print(sh_pass)
+    exit = input("Soddisfatto? ('s' o 'n')\n")
+    if exit == "s":
+        soddisfatto = True
+    else:
+        password = []
